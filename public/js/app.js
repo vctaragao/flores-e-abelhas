fd2aa08343c6f2aa1f33bc546fb587d8d081f51e
Victor Moraes de Aragão <vct.aragao@unifei.edu.br>
2020-05-22 20:10:05 -0300

    JS para a página de cadastrar uma flor

--- public/js/app.js
+++ public/js/app.js
@@ -19287,11 +19287,17 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, {});
   var bees = document.getElementById("bees");
+  var bees_button = document.querySelector('.bees button');
+  var inputs = document.querySelectorAll('form input[type="text"], textarea');
+  console.log(inputs);
   instances[0].input.style.visibility = "hidden";
-  instances[0].input.style.height = "0"; // instances[0].svg.style.display = "none";
-
-  bees.addEventListener('click', function () {
-    console.log(instances[0]);
+  instances[0].input.style.height = "0";
+  bees.addEventListener('click', function (e) {
+    e.preventDefault();
+    instances[0].dropdown.open();
+  });
+  bees_button.addEventListener('click', function (e) {
+    e.preventDefault();
     instances[0].dropdown.open();
   });
   if (instances[0].input.value.length) populateBeesTags(instances[0].input.value.split(','), bees);
@@ -19299,11 +19305,16 @@ document.addEventListener('DOMContentLoaded', function () {
     bees.innerHTML = '';
     populateBeesTags(instances[0].input.value.split(','), bees);
   });
+  inputs.forEach(function (input) {
+    input.addEventListener('focus', function (e, i) {
+      if (this.classList.contains('error')) this.classList.remove('error');
+    });
+  });
 });
 
 function populateBeesTags(bees_selected, bees_element) {
   bees_selected.forEach(function (bee) {
-    var span = document.createElement("span");
+    var span = document.createElement("p");
     span.classList.add('bee_tag');
     span.innerHTML = bee;
     bees_element.appendChild(span);
