837a6c28bcf108d6482f3090c92ef05f9f7ba2e5
Victor Moraes de Aragão <vct.aragao@unifei.edu.br>
2020-05-22 20:09:45 -0300

    Mensagens de erros para a validação

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $error_messages = [
+        'name.required'         => 'O campo de nome é obrigatório',
+        'species.required'      => 'O campo de espécie é obrigatório',
+        'description.required'  => 'O campo de descrição é obrigatório',
+        'months.required'       => 'Marque os meses para essa flor',
+        'bees.required'         => 'Adicione as abelhas para essa flor',
+        'file.required'         => 'Adicione uma imagem a essa flor'
+    ];
 }
