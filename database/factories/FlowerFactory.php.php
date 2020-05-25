a953ee0c06431c433ef7824d21960ea53d96ec35
Victor Moraes de Aragão <vct.aragao@gmail.com>
2020-05-25 18:51:28 -0300

    Adicionando uma factory para não dar problema na instalação do composer

---
+++ database/factories/FlowerFactory.php.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Flower;
+use Faker\Generator as Faker;
+
+$factory->define(Flower::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
