a36b15fc622b22d4f48c5f884ad6a07dc3b661b5
Victor Moraes de Aragão <vct.aragao@gmail.com>
2020-05-25 18:40:31 -0300

    Clonar a master e não a branch development

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -28,7 +28,7 @@ echo 'Checking app dir'
 @task('clone_repository_dev')
 echo 'Cloning repository'
 [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-git clone --depth 1 --branch development {{ $repository }} {{ $new_release_dir }}
+git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
 cd {{ $new_release_dir }}
 git reset --hard {{ $commit }}
 @endtask
